8c7f295945c3ed0a745cbd05445b51aec9481937
Pedrito M Parrilla III <pedritoiii.parrilla@evsu.edu.ph>
2025-12-15 12:58:02 +0800

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -46,7 +46,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "30ef81906bfa86efa17e247d3c9cb1dc
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "a49718d7ef0785db03388fc56c5b9c42",
+"flutter_bootstrap.js": "1b0f0adc6fe94ba2f9306046da40a98e",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
